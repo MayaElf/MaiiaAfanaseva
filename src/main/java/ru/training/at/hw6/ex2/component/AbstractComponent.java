39c485941b8ba7158549c3ebe5c43a79f645b5c7
MayaElf <afanasevaml@yandex.ru>
2021-06-30 13:02:25 +0300

    homework_6_1stAttempt

---
+++ src/main/java/ru/training/at/hw6/ex2/component/AbstractComponent.java
@@ -0,0 +1,17 @@
+package ru.training.at.hw6.ex2.component;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public abstract class AbstractComponent {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    protected AbstractComponent(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+        PageFactory.initElements(driver, this);
+    }
+}
